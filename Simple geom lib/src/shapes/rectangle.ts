f3295cb469f8e0fb3350ccfc75c453886f9ce490
Gizmo657 <90689095+Gizmo657@users.noreply.github.com>
2026-02-06 04:18:06 +0300

    Update rectangle.ts

--- Simple geom lib/src/shapes/rectangle.ts
+++ Simple geom lib/src/shapes/rectangle.ts
@@ -4,6 +4,8 @@ export interface Rectangle extends Shape {
   readonly type: 'rectangle';
   readonly width: number;
   readonly height: number;
+  readonly eventTarget: EventTarget;
+  editShape(newWidth: number, newHeight: number): Rectangle;
   getDiagonal(): number;
   isSquare(): boolean;
   getSides(): number[];
@@ -11,12 +13,26 @@ export interface Rectangle extends Shape {
 
 export class RectangleFactory implements ShapeFactory<Rectangle> {
   create(width: number, height: number): Rectangle {
-    return {
+    const eventTarget = new EventTarget();
+    const rectangle: Rectangle =  {
       type: 'rectangle',
       width,
       height,
+      eventTarget,
       getArea: () => width * height,
       getPerimeter: () => 2 * (width + height),
+      editShape: (newWidth: number, newHeight: number): Rectangle => {
+        if (newWidth <= 0 && newHeight <= 0) {
+          throw new Error('Стороны прямоугольника должны быть больше или равны нулю.');
+        }
+
+        const editEvent = new CustomEvent('rectangle-edited', {
+          detail: { oldWidth: width, newWidth, oldHeight: height, newHeight },
+        });
+        rectangle.eventTarget.dispatchEvent(editEvent);
+
+        return new RectangleFactory().create(newWidth, newHeight);
+      },
       getDiagonal: () => Math.sqrt(width * width + height * height),
       isSquare: () => {
         if (width == height) {
@@ -32,6 +48,8 @@ export class RectangleFactory implements ShapeFactory<Rectangle> {
         return [a, b, a, b];
       }
     };
+
+    return rectangle;
   }
   
   validate(shape: Rectangle): boolean {
