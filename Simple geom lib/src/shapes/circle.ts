37faeaae93aa6a88c2aef50168bbdadde4626456
Gizmo657 <90689095+Gizmo657@users.noreply.github.com>
2026-02-06 04:17:48 +0300

    Update circle.ts

--- Simple geom lib/src/shapes/circle.ts
+++ Simple geom lib/src/shapes/circle.ts
@@ -3,18 +3,36 @@ import { Shape, ShapeFactory } from '../index';
 export interface Circle extends Shape {
   readonly type: 'circle';
   readonly radius: number;
+  readonly eventTarget: EventTarget;
+  editShape(newRadius: number): Circle;
   getDiameter(): number;
 }
 
 export class CircleFactory implements ShapeFactory<Circle> {
   create(radius: number): Circle {
-    return {
+    const eventTarget = new EventTarget();
+    const circle: Circle = {
       type: 'circle',
       radius,
+      eventTarget,
       getArea: () => Math.PI * radius ** 2,
       getPerimeter: () => 2 * Math.PI * radius,
-      getDiameter: () => 2 * radius
+      editShape: (newRadius: number): Circle => {
+        if (newRadius <= 0) {
+          throw new Error('Радиус круга должен быть больше нуля.');
+        }
+
+        const editEvent = new CustomEvent('circle-edited', {
+          detail: { oldRadius: radius, newRadius: newRadius },
+        });
+        circle.eventTarget.dispatchEvent(editEvent);
+
+        return new CircleFactory().create(newRadius);
+      },
+      getDiameter: () => 2 * radius,
     };
+
+    return circle;
   }
   
   validate(shape: Circle): boolean {
